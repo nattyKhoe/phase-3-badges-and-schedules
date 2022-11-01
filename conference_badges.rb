84b00329283a19d7b2fd26521016f131cc374074
Natasha <natasha.khoe@gmail.com>
2022-11-01 19:26:13 +1100

    Done? still confused with puts, p, and pp

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+    array.map do |name|
+        badge_maker (name)
+    end
+end
+
+def assign_rooms (array)
+    new_array = []
+    array.each_with_index do |element, index|
+        new_array << "Hello, #{element}! You'll be assigned to room #{index+1}!"
+    end
+    new_array
+end
+
+def printer (array)
+    batch_badge_creator(array).each{|badge| puts badge}
+    assign_rooms(array).each{|assignment| puts assignment}
+end
+
